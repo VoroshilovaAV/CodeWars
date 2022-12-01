cab62e4a73e6aea4583fd9e3487a4315d8abf3aa
VoroshilovaAV <Voroshilova.AV@yandex.ru>
2022-12-01 22:23:30 +0400

    feat: add "If you cant sleep, just count sheep!!" task solution

---
+++ 8-kyu/If-you-can't-sleep-just count-sheep.js
@@ -0,0 +1,9 @@
+/*
+Description:
+
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+*/
+
+var countSheep = function (num){
+    return num === 0 ? '' : [...Array(num).keys()].map(item => item + 1 + ' sheep...').join('');
+}
