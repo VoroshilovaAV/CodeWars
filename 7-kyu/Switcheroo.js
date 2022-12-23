f5144816ef47e3562890e35bada143fee9d9ddea
VoroshilovaAV <Voroshilova.AV@yandex.ru>
2022-12-23 16:47:37 +0400

    feat: add "Switcheroo" task solution

---
+++ 7-kyu/Switcheroo.js
@@ -0,0 +1,18 @@
+/*
+Description:
+
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+*/
+
+function switcheroo(x){
+    return x.split('').map(item => {
+      if (item === 'a') return 'b';
+      if (item === 'b') return 'a';
+      return item;
+    }).join('');
+  }
